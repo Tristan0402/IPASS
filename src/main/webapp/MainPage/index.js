b5936fb27ae38b5a551957e9248d727218f9bf75
DarkNightNetwork <65709666+DarkNightNetwork@users.noreply.github.com>
2020-06-28 19:29:15 +0200

    Commit

--- src/main/webapp/MainPage/index.js
+++ src/main/webapp/MainPage/index.js
@@ -2,8 +2,11 @@ let ShoppingCartContent = [];
 function AddToCart(Broodjenummer){
     let AmountInCart = ShoppingCartContent.push(Broodjenummer);
     document.getElementById('AantalItems').innerHTML = AmountInCart;
+    alert(ShoppingCartContent);
 }
+
 function BackupInJSON(){
+    sessionStorage.setItem("Cart", ShoppingCartContent);
     ShoppingCartContent = [];
     document.getElementById('AantalItems').innerHTML = ShoppingCartContent.length
 }
---
+++ src/main/webapp/ShoppingCart/ShoppingCart.js
@@ -0,0 +1,5 @@
+function Events() {
+    let ShopCart = sessionStorage.getItem("Cart");
+    document.getElementById('text').innerHTML = "Hey";
+}
+window.onload(Events());
